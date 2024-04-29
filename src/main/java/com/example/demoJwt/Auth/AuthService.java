e3c3ec1995c9ec05827bb982aaf56cbfcb0c8b6e
ElsesserMatias <elsesser3@gmail.com>
2024-04-28 22:30:49 -0300

    add hashing for password

--- src/main/java/com/example/demoJwt/Auth/AuthService.java
+++ src/main/java/com/example/demoJwt/Auth/AuthService.java
@@ -5,6 +5,7 @@ import com.example.demoJwt.User.User;
 import com.example.demoJwt.User.UserRepository;
 import com.example.demoJwt.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +14,12 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthResponse register(RegisterRequest request) {
         User user = User.builder()
                 .username(request.getUsername())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .country(request.getCountry())
